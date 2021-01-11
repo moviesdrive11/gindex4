3f371490a284d3303efdb1e61b07a5eb1e5f7784
moviesdrive11 <77267565+moviesdrive11@users.noreply.github.com>
2021-01-11 15:24:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5479,22 +5479,22 @@
   );
 });
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/css/mdui_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/css/mdui_v2.min.css">'
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/css/nexmoe_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/css/nexmoe_v2.min.css">'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/js/mdui.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/js/mdui.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/js/flv.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.1/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/moviesdrive11/gindex4@1.2/js/markdown-it.min.js"></script>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
